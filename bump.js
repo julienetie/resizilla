b3fdd66eb625455e2fefcc60f7b254caea9b38cb
julienetie <j8@protonmail.com>
2016-11-04 11:38:34 +0000

    Created bump.js to bump package.json and dist/version.js

---
+++ bump.js
@@ -0,0 +1,29 @@
+/** 
+ * Bumps the distribution and package.json
+ */
+const mz = require('mz/fs');
+const colors = require('colors');
+const fs = require('fs');
+const formatJSON = require('format-json');
+const version = process.argv[2];
+
+
+fs.writeFile('./dist/version.js', `export default '${version}';`, (error) => {
+	if (error) return console.log(error);
+	console.log(`\n\n./dist/version.js bumped to '${version}'`.green);
+});
+
+
+mz.readFile('./package.json').then(contents => {
+	let newPk = JSON.parse(contents);
+
+	newPk.version = version;
+	newPk = new Buffer(JSON.stringify(newPk, null, "\t"));
+
+	fs.writeFile('./package.json', newPk, (error) => {
+		if (error) return console.log(error);
+	});
+
+	console.log(`\n\n./package.json bumped to '${version}'\n\n`.green);
+})
+.catch(err => console.error(err));
