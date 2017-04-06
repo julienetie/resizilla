a73b4177dcf303438c6a93e8d33bd6dd578d3910
Julien Etienne <j8@protonmail.ch>
2017-04-06 03:18:00 +0100

    Bumped version

--- dist/resizilla.js
+++ dist/resizilla.js
@@ -19,27 +19,6 @@ vVVv    vVVv                 ': |_| \_\___||___/_/___|_|_|_|\__,_| ''
  Repository: https://github.com/julienetie/resizilla
 */
 
-/*           _.-~-.
-           7''  Q..\
-        _7         (_
-      _7  _/    _q.  /
-    _7 . ___  /VVvv-'_                                            .
-   7/ / /~- \_\\      '-._     .-'                      /       //
-  ./ ( /-~-/  ||'=.__  '::. '-~'' {             ___   /  //     ./{
- V   V-~-~|   ||   __''_   ':::.   ''~-~.___.-'' _/  // / {_   /  {  /
-  VV/-~-~-|  / \ .'__'. '.  '::  ____               _ _ _        ''.
-  / /~~~~||  VVV/ /  \ )  \     |  _ \ ___  ___(_)___(_) | | __ _   .::'
- / (~-~-~\\.-' /    \'   \::::. | |_) / _ \/ __| |_  / | | |/ _` | :::'
-/..\    /..\__/      '     '::: |  _ <  __/\__ \ |/ /| | | | (_| | ::'
-vVVv    vVVv                 ': |_| \_\___||___/_/___|_|_|_|\__,_| ''
-*/
-/*
- Version: 0.9.1
- Description: A Better Window Resize
- Author: Julien Etienne
- Repository: https://github.com/julienetie/resizilla
-*/
-
 /**
  * request-frame-modern - Optimal requestAnimationFrame & cancelAnimationFrame polyfill for modern development
  * @version v2.0.3
