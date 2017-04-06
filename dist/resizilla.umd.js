39071233e7485c3fdddd184862d158b699dfd8ae
Julien Etienne <j8@protonmail.ch>
2017-04-06 02:39:06 +0100

    Bumped version

--- dist/resizilla.umd.js
+++ dist/resizilla.umd.js
@@ -1,3 +1,24 @@
+/*           _.-~-.
+           7''  Q..\
+        _7         (_
+      _7  _/    _q.  /
+    _7 . ___  /VVvv-'_                                            .
+   7/ / /~- \_\\      '-._     .-'                      /       //
+  ./ ( /-~-/  ||'=.__  '::. '-~'' {             ___   /  //     ./{
+ V   V-~-~|   ||   __''_   ':::.   ''~-~.___.-'' _/  // / {_   /  {  /
+  VV/-~-~-|  / \ .'__'. '.  '::  ____               _ _ _        ''.
+  / /~~~~||  VVV/ /  \ )  \     |  _ \ ___  ___(_)___(_) | | __ _   .::'
+ / (~-~-~\\.-' /    \'   \::::. | |_) / _ \/ __| |_  / | | |/ _` | :::'
+/..\    /..\__/      '     '::: |  _ <  __/\__ \ |/ /| | | | (_| | ::'
+vVVv    vVVv                 ': |_| \_\___||___/_/___|_|_|_|\__,_| ''
+*/
+/*
+ Version: 0.9.1
+ Description: A Better Window Resize
+ Author: Julien Etienne
+ Repository: https://github.com/julienetie/resizilla
+*/
+
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
